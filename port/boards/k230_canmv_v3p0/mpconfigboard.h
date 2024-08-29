910f15c801a748771c46f891765919ab228f30bc
kendryte747 <kendryte747@gmail.com>
2024-08-29 09:48:24 +0800

    Add new board K230_CanMV_V3.0

---
+++ port/boards/k230_canmv_v3p0/mpconfigboard.h
@@ -0,0 +1,4 @@
+#define MICROPY_HW_BOARD_NAME               "k230_canmv_v3p0"
+#define MICROPY_HW_MCU_NAME                 "K230"
+#define OMV_ARCH_STR                        ""
+#define OMV_BOARD_TYPE                      "CanMV K230 V3.0"
