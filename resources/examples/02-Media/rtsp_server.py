cdb98f8e4bc26b44fd73e731aac95979eae704fc
gh-sxp <sunxiaopeng@canaan-creative.com>
2024-09-12 15:01:59 +0800

    modify rtsp demo

--- resources/examples/02-Media/rtsp_server.py
+++ resources/examples/02-Media/rtsp_server.py
@@ -4,33 +4,125 @@
 #
 # You can run the rtsp server to stream video and audio to the network
 
+from media.vencoder import *
+from media.sensor import *
+from media.media import *
+import time, os
+import _thread
+import multimedia as mm
 from time import *
-from media.rtspserver import *   #导入rtsp server 模块
-import os
-import time
 
-def rtsp_server_test():
-    rtspserver = RtspServer(session_name="test",port=8554,enable_audio=False) #创建rtsp server对象
-    rtspserver.start() #启动rtsp server
-    print("rtsp server start:",rtspserver.get_rtsp_url()) #打印rtsp server start
+class RtspServer:
+    def __init__(self,session_name="test",port=8554,video_type = mm.multi_media_type.media_h264,enable_audio=False):
+        self.session_name = session_name # session name
+        self.video_type = video_type  # 视频类型264/265
+        self.enable_audio = enable_audio # 是否启用音频
+        self.port = port   #rtsp 端口号
+        self.rtspserver = mm.rtsp_server() # 实例化rtsp server
+        self.venc_chn = VENC_CHN_ID_0 #venc通道
+        self.start_stream = False #是否启动推流线程
+        self.runthread_over = False #推流线程是否结束
+
+    def start(self):
+        # 初始化推流
+        self._init_stream()
+        self.rtspserver.rtspserver_init(self.port)
+        # 创建session
+        self.rtspserver.rtspserver_createsession(self.session_name,self.video_type,self.enable_audio)
+        # 启动rtsp server
+        self.rtspserver.rtspserver_start()
+        self._start_stream()
+
+        # 启动推流线程
+        self.start_stream = True
+        _thread.start_new_thread(self._do_rtsp_stream,())
 
 
-    #运行30s
-    time_start = time.time() #获取当前时间
-    try:
-        while(time.time() - time_start < 30):
-            time.sleep(0.1)
-            os.exitpoint()
-    except KeyboardInterrupt as e:
-        print("user stop: ", e)
-    except BaseException as e:
-        import sys
-        sys.print_exception(e)
+    def stop(self):
+        # 等待推流线程退出
+        self.start_stream = False
+        while not self.runthread_over:
+            sleep(0.1)
+        self.runthread_over = False
 
-    rtspserver.stop() #停止rtsp server
-    print("rtsp server stop") #打印rtsp server stop
+        # 停止推流
+        self._stop_stream()
+        self.rtspserver.rtspserver_stop()
+        #self.rtspserver.rtspserver_destroysession(self.session_name)
+        self.rtspserver.rtspserver_deinit()
+
+    def get_rtsp_url(self):
+        return self.rtspserver.rtspserver_getrtspurl(self.session_name)
+
+    def _init_stream(self):
+        width = 1280
+        height = 720
+        width = ALIGN_UP(width, 16)
+        # 初始化sensor
+        self.sensor = Sensor()
+        self.sensor.reset()
+        self.sensor.set_framesize(width = width, height = height, alignment=12)
+        self.sensor.set_pixformat(Sensor.YUV420SP)
+        # 实例化video encoder
+        self.encoder = Encoder()
+        self.encoder.SetOutBufs(self.venc_chn, 8, width, height)
+        # 绑定camera和venc
+        self.link = MediaManager.link(self.sensor.bind_info()['src'], (VIDEO_ENCODE_MOD_ID, VENC_DEV_ID, self.venc_chn))
+        # init media manager
+        MediaManager.init()
+        # 创建编码器
+        chnAttr = ChnAttrStr(self.encoder.PAYLOAD_TYPE_H264, self.encoder.H264_PROFILE_MAIN, width, height)
+        self.encoder.Create(self.venc_chn, chnAttr)
+
+    def _start_stream(self):
+        # 开始编码
+        self.encoder.Start(self.venc_chn)
+        # 启动camera
+        self.sensor.run()
+
+    def _stop_stream(self):
+        # 停止camera
+        self.sensor.stop()
+        # 接绑定camera和venc
+        del self.link
+        # 停止编码
+        self.encoder.Stop(self.venc_chn)
+        self.encoder.Destroy(self.venc_chn)
+        # 清理buffer
+        MediaManager.deinit()
+
+    def _do_rtsp_stream(self):
+        try:
+            streamData = StreamData()
+            while self.start_stream:
+                os.exitpoint()
+                # 获取一帧码流
+                self.encoder.GetStream(self.venc_chn, streamData)
+                # 推流
+                for pack_idx in range(0, streamData.pack_cnt):
+                    stream_data = bytes(uctypes.bytearray_at(streamData.data[pack_idx], streamData.data_size[pack_idx]))
+                    self.rtspserver.rtspserver_sendvideodata(self.session_name,stream_data, streamData.data_size[pack_idx],1000)
+                    #print("stream size: ", streamData.data_size[pack_idx], "stream type: ", streamData.stream_type[pack_idx])
+                # 释放一帧码流
+                self.encoder.ReleaseStream(self.venc_chn, streamData)
+
+        except KeyboardInterrupt as e:
+            print("user stop: ", e)
+        except BaseException as e:
+            sys.print_exception(e)
+
+        self.runthread_over = True
 
 if __name__ == "__main__":
     os.exitpoint(os.EXITPOINT_ENABLE)
-    rtsp_server_test()
-    print("rtsp server done")
+    # 创建rtsp server对象
+    rtspserver = RtspServer()
+    # 启动rtsp server
+    rtspserver.start()
+    # 打印rtsp url
+    print("rtsp server start:",rtspserver.get_rtsp_url())
+    # 推流60s
+    sleep(60)
+    # 停止rtsp server
+    rtspserver.stop()
+    print("done")
