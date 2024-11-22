6a31049e5f02f362f622eb9fc41a5a17e505dcc6
kendryte747 <kendryte747@gmail.com>
2024-11-22 11:27:54 +0800

    Fix k230d build failed.

--- port/boards/k230d_canmv_atk_dnk230d/mpconfigboard.h
+++ port/boards/k230d_canmv_atk_dnk230d/mpconfigboard.h
@@ -1,4 +1,4 @@
 #define MICROPY_HW_BOARD_NAME               "k230d_canmv_atk_dnk230d"
 #define MICROPY_HW_MCU_NAME                 "K230d"
 #define OMV_ARCH_STR                        ""
-#define OMV_BOARD_TYPE                      "CanMV K230D ATK-DNK230D"
+#define OMV_BOARD_TYPE                      "CanMV K230D ATK-DNK230D - %d%c"
